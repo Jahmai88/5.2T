65a63bb97d803223f39b52b1f31630a261864ba9
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 20:10:06 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Enter the age \n");
+        Scanner sc = new Scanner(System.in);
+        String Age = sc.next();
+        int Age = sc.nextInt();
+        int Fare;
+        if (Age <= 11); { Fare = 3;}
+        System.out.print("Fare $3\n");
+        String Fu = sc.next();
+
+}
+    }
+
